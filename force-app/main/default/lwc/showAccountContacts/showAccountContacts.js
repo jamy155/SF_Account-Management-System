bb0d583673c2e77964c4f7b2f40fdbab0a720672
jamy155 <jarhudaugustin@gmail.com>
2025-04-12 02:03:01 -0400

    ADDED MessageService to accountList & showAccountContacts Component

--- force-app/main/default/lwc/showAccountContacts/showAccountContacts.js
+++ force-app/main/default/lwc/showAccountContacts/showAccountContacts.js
@@ -1,3 +1,39 @@
-import { LightningElement } from 'lwc';
+import { MessageContext, subscribe } from 'lightning/messageService';
+import { LightningElement , wire } from 'lwc';
+import Comrevo from '@salesforce/messageChannel/Comrevo__c';
 
-export default class ShowAccountContacts extends LightningElement {}
+
+export default class ShowAccountContacts extends LightningElement {
+
+    subscription = null;
+    @wire (MessageContext) messageContext;
+
+    accountId;
+    accountName;
+
+    connectedCallback() {
+        this.handleSubscribe();
+    }
+
+    disconnectedCallback() {
+        this.handleUnsubscribe();
+    }
+
+    handleSubscribe() {
+        if(!this.subscription) 
+            {
+                this.subscription = subscribe(this.messageContext,Comrevo, 
+                    (param)=>
+                        {
+                            this.accountId = param.accountId;
+                            this.accountName = param.accountName;
+                        })
+            
+            }
+    }
+
+    handleUnsubscribe() {
+        unsubscribe(this.subscription);
+        this.subscription = null;
+    }
+}
