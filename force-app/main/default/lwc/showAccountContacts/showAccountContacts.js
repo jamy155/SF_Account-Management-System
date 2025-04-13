2f1ce09485334f68ec266aaee9837ba61a5cff2f
jamy155 <jarhudaugustin@gmail.com>
2025-04-13 07:02:00 -0400

    ADDED getAccContacts() method to AccountClass AND ADDED account's contacts names on the showAccountContacts component

--- force-app/main/default/lwc/showAccountContacts/showAccountContacts.js
+++ force-app/main/default/lwc/showAccountContacts/showAccountContacts.js
@@ -1,11 +1,16 @@
 import { MessageContext, subscribe } from 'lightning/messageService';
 import { LightningElement , wire } from 'lwc';
 import Comrevo from '@salesforce/messageChannel/Comrevo__c';
+import getAccContacts from '@salesforce/apex/AccountClass.getAccContacts';
 
 
 export default class ShowAccountContacts extends LightningElement {
 
     subscription = null;
+    title = "Contacts";
+    contacts;
+    hasContacts;
+
     @wire (MessageContext) messageContext;
 
     accountId;
@@ -27,6 +32,8 @@ export default class ShowAccountContacts extends LightningElement {
                         {
                             this.accountId = param.accountId;
                             this.accountName = param.accountName;
+                            this.title = param.accountName+"'s Contacts";
+                            this.getContacts();
                         })
             
             }
@@ -35,5 +42,11 @@ export default class ShowAccountContacts extends LightningElement {
     handleUnsubscribe() {
         unsubscribe(this.subscription);
         this.subscription = null;
+    }
+
+    async getContacts() {
+        this.contacts =  await getAccContacts({accId: this.accountId});
+        this.hasContacts = this.contacts.length > 0? true:false;
+        
     }
 }
